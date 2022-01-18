7f95bb288378f6a1b68055097e88692e797fe019
gahnsei <gahnsei@gmail.com>
2022-01-18 15:44:55 -0600

    Working Code

--- calc.js
+++ calc.js
@@ -14,7 +14,35 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	let x = 4;
+	let ansr;
 
+	switch (mathSymbol){
+		case `+`: 
+		ansr = num1 + num2;
+		break;
+
+		case `-`: 
+		ansr = num1 - num2;
+		break;
+
+		case `*`: 
+		ansr = num1 * num2;
+		break;
+
+		case `/`: 
+		ansr = num1 / num2;
+		break;
+
+		case `sqrt`: 
+		ansr = Math.sqrt(num1);
+		break;
+
+		default:
+		ansr = `Niether`;
+	}
+
+console.log(ansr);
 	// This line closes the connection to the command line interface.
 	reader.close()
 
